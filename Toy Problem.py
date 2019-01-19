4a217f754c79bd5f5de51d51373c8bcc677c6b2b
Gavztheouch <32553777+gavztheouch@users.noreply.github.com>
2019-01-19 21:41:20 +0000

    Update Toy Problem.py

--- Toy Problem.py
+++ Toy Problem.py
@@ -1,8 +1,10 @@
 import cv2       #import opencv needed for webcam
+import time
 import matplotlib.pyplot as plt    #matplotlib to show images
 
 
 video_capture = cv2.VideoCapture(0)
+time.sleep(3)
 ret, frame = video_capture.read()
 plt.imshow(frame)
 plt.show() 
