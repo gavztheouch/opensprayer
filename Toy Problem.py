014f642eaeaae623ca7227d229748c2817114a83
Gavztheouch <32553777+gavztheouch@users.noreply.github.com>
2019-01-19 22:46:21 +0000

    Update Toy Problem.py

--- Toy Problem.py
+++ Toy Problem.py
@@ -4,11 +4,19 @@ import matplotlib.pyplot as plt    #matplotlib to show images
 
 
 video_capture = cv2.VideoCapture(0)
-time.sleep(3)
+time.sleep(1)
 ret, frame = video_capture.read()
-crop_img = frame[0:256, 0:256]     #this is numpy slicing x and y coords for box
+crop_img = frame[0:256, 0:256]     #this is numpy slicing
+gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY )   #convert to grayscale
+ret,im_bw = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 plt.imshow(frame)
 plt.show() 
-plt.imshow(crop_img)
+plt.imshow(gray, cmap=plt.cm.gray)
+plt.show() 
+plt.imshow(im_bw, cmap=plt.cm.gray)
 plt.show() 
 video_capture.release()
+
+cv2.imshow('image',im_bw)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
