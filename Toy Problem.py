c66ad9af3c907046a21ffc26efd1038da837da51
Gavztheouch <32553777+gavztheouch@users.noreply.github.com>
2019-01-19 21:46:47 +0000

    Update Toy Problem.py

--- Toy Problem.py
+++ Toy Problem.py
@@ -6,6 +6,9 @@ import matplotlib.pyplot as plt    #matplotlib to show images
 video_capture = cv2.VideoCapture(0)
 time.sleep(3)
 ret, frame = video_capture.read()
+crop_img = frame[0:256, 0:256]     #this is numpy slicing x and y coords for box
 plt.imshow(frame)
+plt.show() 
+plt.imshow(crop_img)
 plt.show() 
 video_capture.release()
