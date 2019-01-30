1022ec3cfc5adb8dacfae4688afcdb0ecfeed1ad
Gavztheouch <32553777+gavztheouch@users.noreply.github.com>
2019-01-30 18:51:27 +0000

    Update rotating deep learning barrel.py

--- rotating deep learning barrel.py
+++ rotating deep learning barrel.py
@@ -1,1 +1,32 @@
+from skimage.filters import threshold_local
+import numpy as np
+import argparse
+import cv2
+import imutils
+import matplotlib.pyplot as plt
 
+# load the image and compute the ratio of the old height
+# to the new height, clone it, and resize it
+image = cv2.imread("Gavin.jpg")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.GaussianBlur(gray, (5, 5), 0)
+edged = cv2.Canny(gray, 75, 200)
+
+plt.imshow(image)
+plt.show()
+
+
+
+# convert the image to grayscale, blur it, and find edges
+# in the image
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.GaussianBlur(gray, (5, 5), 0)
+edged = cv2.Canny(gray, 75, 200)
+plt.imshow(edged)
+plt.show() 
+# show the original image and the edge detected image
+print("STEP 1: Edge Detection")
+#cv2.imshow("Image", image)
+#cv2.imshow("Edged", edged)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
